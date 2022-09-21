f26e433b4befd0ac6d6f3aba30bc2058044f8c01
Alex <aliakseishulha2021@gmail.com>
2022-09-21 18:58:22 +0300

    render array categories

--- src/components/Categories.tsx
+++ src/components/Categories.tsx
@@ -1,15 +1,29 @@
-import React from "react";
+import React, {useState} from "react";
 
 export const Categories = () => {
+    const [activeIndex, setActiveIndex] = useState(0);
+
+    const categories = [
+        'Все',
+        'Мясные',
+        'Вегетарианская',
+        'Гриль',
+        'Острые',
+        'Закрытые',
+    ]
+
+    const onClickCategory = (index: number) => {
+        setActiveIndex(index)
+    }
     return (
         <div className="categories">
             <ul>
-                <li className="active">Все</li>
-                <li className="">Мясные</li>
-                <li className="">Вегетарианская</li>
-                <li className="">Гриль</li>
-                <li className="">Острые</li>
-                <li className="">Закрытые</li>
+                {categories.map((el, index) => (
+                    <li key={index} onClick={() => onClickCategory(index)}
+                        className={activeIndex === index ? "active" : ""}>
+                        {el}
+                    </li>
+                ))}
             </ul>
         </div>
     )
