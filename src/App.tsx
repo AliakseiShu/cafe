903a93bfcea646faa3472c29a28b83542f7f8467
Alex <aliakseishulha2021@gmail.com>
2022-09-22 12:19:13 +0300

    used fetch

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,36 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
 import {Header} from "./components/Header";
 import {Categories} from "./components/Categories";
 import {Sort} from "./components/Sort";
 import {PizzaBlock} from "./components/PizzaBlock";
-import pizzas from "./assets/pizza.json";
 
 import './scss/app.scss';
-
 export const App = () => {
+    const [items, setItems] = useState<any[]>([]);
+    console.log(items)
+    useEffect(() => {
+        fetch('https://632c1cb15568d3cad87cfbac.mockapi.io/items').then((response) => {
+            return response.json()
+        }).then(arr => {
+            setItems(arr)
+
+        })
+    },[])
+
     return (
         <div className="wrapper">
             <Header/>
             <div className="content">
                 <div className="container">
                     <div className="content__top">
-                        {/*<Categories/>*/}
+                        <Categories/>
                         <Sort/>
                     </div>
                     <h2 className="content__title">Все пиццы</h2>
                     <div className="content__items">
-                        {pizzas.map((pizza) => (
-                            <PizzaBlock key={pizza.id} {...pizza}/>
+                        {items.map((item) => (
+                            <PizzaBlock key={item.id} {...item}/>
                         ))}
                     </div>
                     {/*<Pagination/>*/}
