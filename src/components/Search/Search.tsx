969a5639a04cb0ba8c7e6dca81510b6e873e4875
Alex <aliakseishulha2021@gmail.com>
2022-09-24 19:37:41 +0300

    create Search scss

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import styles from './Search.module.scss';
+import searchSVG from '../../assets/img/search_icon.svg'
 
 export const Search = () => {
     return (
-        <div>
-            
+        <div className={styles.root}>
+            <img className={styles.icon} src={searchSVG} alt="Search"/>
+            <input className={styles.input} placeholder="Поиск пиццы ..."/>
         </div>
     );
 };
