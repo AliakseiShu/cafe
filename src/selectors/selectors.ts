fd6f503486da91b02cf1925b96d7c9d20e9ced2d
Alex <aliakseishulha2021@gmail.com>
2022-10-07 23:15:47 +0300

    fixed useAppSelector

--- src/selectors/selectors.ts
+++ src/selectors/selectors.ts
@@ -1,9 +1,8 @@
 import {RootState} from "../redux/store";
-import {useSelector} from "react-redux";
 
 export const selectTotalPrice = (state: RootState) => state.cart.totalPrice
 export const selectItems = (state: RootState) => state.cart.items
 export const selectCategoryId = (state: RootState) => state.filter.categoryId
-export const selectSortProperty = (state: RootState) => state.filter.sort.sortProperty
+//export const selectSortProperty = (state: RootState) => state.filter.sort.sortProperty
 
 
