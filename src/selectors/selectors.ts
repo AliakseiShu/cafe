aa359732427c00c2395c8d15e89f3dffb9f14518
Alex <aliakseishulha2021@gmail.com>
2022-10-10 16:15:16 +0300

    clean code

--- src/selectors/selectors.ts
+++ src/selectors/selectors.ts
@@ -3,6 +3,6 @@ import {RootState} from "../redux/store";
 export const selectTotalPrice = (state: RootState) => state.cart.totalPrice
 export const selectItems = (state: RootState) => state.cart.items
 export const selectCategoryId = (state: RootState) => state.filter.categoryId
-//export const selectSortProperty = (state: RootState) => state.filter.sort.sortProperty
+
 
 
