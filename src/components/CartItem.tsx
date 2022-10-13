13d1107fcf693834d6926fee67982888c8a469f4
Alex <aliakseishulha2021@gmail.com>
2022-10-13 10:37:59 +0300

    install clsx

--- src/components/CartItem.tsx
+++ src/components/CartItem.tsx
@@ -39,8 +39,10 @@ export const CartItem: FC<CartItemsType> = ({id, title, price, type, size, count
                 <h3>{title}</h3>
                 <p>{type}, {size} см.</p></div>
             <div className="cart__item-count">
-                <button onClick={onClickMinus}
-                        className="button button--outline button--circle cart__item-count-minus">
+                <button
+                    disabled={count === 1}
+                    onClick={onClickMinus}
+                    className="button button--outline button--circle cart__item-count-minus ">
                     <svg width="10" height="10" viewBox="0 0 10 10" fill="none"
                          xmlns="http://www.w3.org/2000/svg">
                         <path
